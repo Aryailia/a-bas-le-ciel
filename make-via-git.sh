8e06bcfe1030342251a3851d4fdc7893ad23013f
Aryailia <Aryailia@users.noreply.github.com>
2021-11-20 10:33:52 +0000

    must be via git

--- make-via-git.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-
-my_dir="$( dirname "${0}"; printf a )"; my_dir="${my_dir%?a}"
-cd "${my_dir}" || exit "$?"
-
-git show main:make.sh | sh -s "$@"
-
--- make.sh
+++ make.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#/bin/sh
 
-../ablc-main/make.sh "$@"
+my_dir="$( dirname "${0}"; printf a )"; my_dir="${my_dir%?a}"
+cd "${my_dir}" || exit "$?"
+
+git fetch origin main
+git show origin/main:make.sh | sh -s "$@"
+
