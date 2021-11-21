8cbdf6f57a90fdeb049b4f4c7f57dcce4a078c10
Aryailia <Aryailia@users.noreply.github.com>
2021-11-21 16:15:18 +0400

    Add make via local branches (see gh-pages repo)

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 my_dir="$( dirname "${0}"; printf a )"; my_dir="${my_dir%?a}"
 cd "${my_dir}" || exit "$?"
 
-git fetch origin main
-git show origin/main:make.sh | sh -s "$@"
+git fetch local main
+git show local/main:make.sh | sh -s "$@"
 
